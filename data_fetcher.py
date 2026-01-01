f0b78792aaa78e2a82c8dd06b08d588942465508
Arnav Agarwal <arnavagarwal05@gmail.com>
2026-01-01 17:30:25 +0530

    Final Edit

--- data_fetcher.py
+++ data_fetcher.py
@@ -15,4 +15,19 @@ for i in range(0,len(data)):
     time.sleep(0.8)
     counter += 1
     print(f'{counter}/{len(data)}',end='\r')
+
+data = pd.read_csv("data/train(1).csv")
+
+# https://github.com/dietrichmax/docker-staticmaps
+counter = 0
+for i in range(0,len(data)):
+    l1 = data['lat'][i]
+    l2 = data['long'][i]
+    query = f"http://localhost:3000/api/staticmaps?width=512&height=512&center={l1},{l2}&zoom=30&basemap=satellite"
+    response = requests.get(query)
+    with open(f"images/{data['id'][i]}.png", "wb") as f:
+        f.write(response.content)
+    time.sleep(0.8)
+    counter += 1
+    print(f'{counter}/{len(data)}',end='\r')
 # docker run -d -p 3000:3000 staticmaps 
